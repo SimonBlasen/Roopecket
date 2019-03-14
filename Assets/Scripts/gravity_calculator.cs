241e6fd60062ac20721398914a9003e613eab98e
ZiosXX <marc-bendtx@gmx.de>
2019-03-14 19:01:00 +0100

    Added Gravity view and modifed terrain levels

---
+++ Assets/Scripts/gravity_calculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity_calculator : MonoBehaviour {
+
+    public string gravity = "-1";
+    public GUISkin skin;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.skin = skin;
+
+        GUI.Label(new Rect (20,20,200,100), "Gravity: " + gravity);
+
+    }
+}
