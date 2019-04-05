ecfe5d45cdac4e1777deb81f4214cf51a9453290
ZiosXX <marc-bendtx@gmx.de>
2019-04-05 12:49:21 +0200

    fixed level 9 camera, added music to all levels, added not working flying goal area, worked on main menu, small fixes

--- Assets/Scripts/gravity_calculator.cs
+++ Assets/Scripts/gravity_calculator.cs
@@ -6,6 +6,7 @@ public class gravity_calculator : MonoBehaviour {
 
     public string gravity = "-1";
     public GUISkin skin;
+    public int showLevel = 0; 
 
     // Use this for initialization
     void Start () {
@@ -16,7 +17,8 @@ public class gravity_calculator : MonoBehaviour {
     {
         GUI.skin = skin;
 
-        GUI.Label(new Rect (20,20,200,100), "Gravity: " + gravity);
+        GUI.Label(new Rect(20, 20, 200, 100), "Level " + showLevel.ToString());
+        GUI.Label(new Rect (20,50,200,100), "Gravity: " + gravity);
 
     }
 }
