8e321cfa8d4b023cd1e66c7cb3744a51ed0485be
ZiosXX <marc-bendtx@gmx.de>
2019-04-14 23:03:50 +0200

    Worked on the main menu

---
+++ Assets/Scripts/MainMenuLogo.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuLogo: MonoBehaviour {
+
+
+    public bool Entered = false;
+    private bool MenuGo = false;
+    
+
+    
+
+    public float seconds = 10;
+    public float timer;
+    public Vector3 Point;
+    public Vector3 Difference;
+    public Vector3 start;
+    public float percent;
+
+
+
+    void Start()
+    {
+        start = transform.position;
+        Point = new Vector3(5, 5, 5);
+        Difference = Point - start;
+    }
+
+    void Update()
+    { 
+        if (Entered)
+        {
+
+            
+
+            if (tag == "Logo")
+            {
+                if (timer <= seconds)
+                {
+
+                    timer += Time.deltaTime;
+
+                    percent = timer / seconds;
+
+
+                   
+                    transform.position = start + Difference * percent;
+                }
+            }
+
+        /*    if (tag == "MenuPoints")
+            {
+
+                print("Step 2");
+                menuText.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+                MenuGo = true;
+                
+
+            }
+
+        */
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Step 1");
+        Entered = true;
+
+    }
+
+}
+
+
---
+++ Assets/Scripts/MainMenuText.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuText : MonoBehaviour {
+
+    private RectTransform menuText;
+    private Vector3 spawn;
+    private Vector3 selectedGarage, selectedContinue, selectedOptions;
+    private bool MouseHover = false;
+    private bool Entered;
+    private bool MousePress = false;
+    public string pointName;
+
+    // Use this for initialization
+    void Start () {
+        spawn = transform.position;
+        selectedContinue = new Vector3(0f,0f,-4.15f);
+        selectedGarage = new Vector3(0f, 0.1f, -4.15f);
+        selectedOptions = new Vector3(0f, -0.1f, -4.15f);
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        
+		
+        if (MouseHover)
+        {
+            if (pointName == "Continue")
+            {
+                transform.position = selectedContinue;
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.color = new Color(1f, 1f, 0.5f, 1f);
+
+                if(MousePress)
+                {
+
+                    SceneManager.LoadScene("Platform First Level");
+
+                }
+
+            }
+
+            if (pointName == "Garage")
+            {
+                transform.position = selectedGarage;
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.color = new Color(1f, 1f, 0.5f, 1f);
+            }
+
+            if (pointName == "Options")
+            {
+                transform.position = selectedOptions;
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.color = new Color(1f, 1f, 0.5f, 1f);
+            }
+
+        }
+        else
+        {
+            transform.position = spawn;
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = new Color(1f, 1f, 1f, 1f);
+        }
+        
+
+	}
+
+    private void OnMouseEnter()
+    {
+        MouseHover = true;
+        print("OKIOKI");
+     //menuText.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        MousePress = true;
+    }
+
+    private void OnMouseExit()
+    {
+        MouseHover = false;
+    }
+
+}
