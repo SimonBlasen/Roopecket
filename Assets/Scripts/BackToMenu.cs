7d56ef9e85f89dfc8dccdbfcdf1a87c7b4daadf3
ZiosXX <marc-bendtx@gmx.de>
2019-05-16 22:02:55 +0200

    Map 18 half completed, Back from garage

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour {
+
+    private Vector3 spawn;
+    public Vector3 selectedText;
+
+    private bool click = false;
+    private bool mouseHover = false;
+
+
+    // Use this for initialization
+    void Start () {
+        spawn = transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (mouseHover)
+        {
+
+            transform.position = selectedText;
+
+            if (click)
+            {
+
+                SceneManager.LoadScene("Main_Menu_3");
+
+            }
+
+        }
+
+        else
+        {
+
+            transform.position = spawn;
+
+        }
+
+	}
+
+    private void OnMouseDown()
+    {
+        click = true;
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseHover = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseHover = false;
+    }
+}
---
+++ Assets/Scripts/PauseContinue.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseContinue : MonoBehaviour {
+
+   
+
+    private bool showPauseScreen = false;
+    private bool menuGO = false;
+
+    public int WSH = 20, WSW = 20;
+
+    public float startTime;
+    private string currentTime;
+    public Rect timerRect;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        timerRect = new Rect(10, 10, 400, 100);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(menuGO)
+        {
+            showPauseScreen = true;
+        }
+
+        startTime += Time.deltaTime;
+        currentTime = string.Format("{0:0.0}",startTime);
+
+
+        
+
+	}
+
+    private void OnGUI()
+    {
+
+        if (showPauseScreen)
+        {
+            print("Pause");
+            Rect winScreenRect = new Rect(Screen.width / 2 - (WSW / 2), Screen.height / 2 - (WSH / 2), WSW, WSH);
+
+            Time.timeScale = 0;
+
+            GUI.Box(winScreenRect, "PAUSE");
+
+            if (GUI.Button(new Rect((winScreenRect.x + winScreenRect.width - 170), (winScreenRect.y + winScreenRect.height - 60), 150, 40), "Main Menu"))
+            {
+                SceneManager.LoadScene("Main_Menu_3");
+            }
+        }
+
+        GUI.Label(timerRect, currentTime);
+    }
+
+    private void OnMouseDown()
+    {
+        menuGO = true;
+    }
+}
