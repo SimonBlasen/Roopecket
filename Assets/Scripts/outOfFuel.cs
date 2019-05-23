a7edbbb638f543a46c2f83467a7d7a7447795225
ZiosXX <marc-bendtx@gmx.de>
2019-05-23 11:01:20 +0200

    MarcsRocket -> easyer to handle, out of fuel screen

---
+++ Assets/Scripts/outOfFuel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class outOfFuel : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject noFuelMenuUI;
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKeyDown(KeyCode.Tab)) // FUEL OUT?
+        {
+            noFuelMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+
+        }
+
+
+
+    }
+
+
+
+    public void Retry()
+    {
+
+        noFuelMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Main_Menu_3");
+
+    }
+
+    public void loadMenu()
+    {
+        noFuelMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu_3");
+
+    }
+
+    
+}
