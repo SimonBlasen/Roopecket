a04beb83cd6d856c36370f5c6e2545340b4832bd
ZiosXX <marc-bendtx@gmx.de>
2019-05-24 16:26:11 +0200

    outOfFuel with no fuel connection, outOfLife with no life connection...

---
+++ Assets/Scripts/rocketDestroyed.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rocketDestroyed : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject noLifeMenuUI;
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Tab)) // FUEL OUT?
+        {
+            noLifeMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+
+        }
+
+
+
+    }
+
+    
+
+    public void Retry()
+    {
+
+        noLifeMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Main_Menu_3");
+
+    }
+
+    public void loadMenu()
+    {
+        noLifeMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu_3");
+
+    }
+
+}
