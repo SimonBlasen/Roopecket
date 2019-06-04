62fa605469238278599230cfc69a63239f365448
SimonBlasen <simon.yzf@t-online.de>
2019-06-04 10:00:45 +0200

    Started working on saving game

---
+++ Assets/Scripts/SavedGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SavedGame
+{
+    // Config variables
+
+    public const string savegameDir = ".";
+
+
+
+    // SavedGame variables
+    public static int LastSelectedRocket = 0;
+    public static int CurrentLevelIndex = 0;
+    public static bool[] OwnedRockets;
+    public static int Money = 0;
+    // Hier kannst noch so viele Sachen hinzufügen wie du willst
+
+
+    public static void LoadSavegame()
+    {
+        if (Directory.Exists(savegameDir) == false)
+        {
+            Debug.LogError("Directory for savegame (" + savegameDir + ") doesn't exist");
+        }
+        else
+        {
+
+        }
+    }
+
+
+
+    private static void writeStringToFile(string text, string filePath)
+    {
+
+    }
+
+    
+}
