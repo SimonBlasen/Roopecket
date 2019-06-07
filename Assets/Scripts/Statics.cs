0ed5cba6d931db0aafbc295c10ec71b43ca1dfaf
SimonBlasen <simon.yzf@t-online.de>
2019-06-07 12:01:30 +0200

    Worked on coop

--- Assets/Scripts/Statics.cs
+++ Assets/Scripts/Statics.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class Statics
 {
     public static int selectedRocket = 0;
+    public static ulong Steam64ID = 0;
+    public static string SteamName = "No Steam name";
 }
