16b059be06deaf8d2dbdec0af67c8e9ec2b37a40
SimonBlasen <simon.yzf@t-online.de>
2019-06-22 20:28:46 +0200

    ParticleSystemPositioning fix

--- Assets/Scripts/ParticleSystemPositioning.cs
+++ Assets/Scripts/ParticleSystemPositioning.cs
@@ -9,23 +9,46 @@ public class ParticleSystemPositioning : MonoBehaviour {
     [SerializeField]
     private Transform[] rockets;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    
+    private RocketSpawner rs;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rs = GameObject.FindObjectOfType<RocketSpawner>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-
-        Vector3 mid = Vector3.zero;
-        
-
-        for (int i = 0; i < rockets.Length; i++)
+	void Update ()
+    {
+        if (rockets.Length > 0 && rockets[0] == null)
         {
-            mid += rockets[i].position;
+            if (rs != null)
+            {
+                if (rs.SpawnedRocket != null)
+                {
+                    rockets = new Transform[] { rs.SpawnedRocket.transform };
+                }
+            }
+            else
+            {
+                rockets = new Transform[] { GameObject.FindObjectOfType<RocketProps>().transform };
+            }
         }
-        mid /= rockets.Length;
+        else
+        {
+            Vector3 mid = Vector3.zero;
 
-        transform.position = mid + offset;
+
+            for (int i = 0; i < rockets.Length; i++)
+            {
+                mid += rockets[i].position;
+            }
+            mid /= rockets.Length;
+
+            transform.position = mid + offset;
+        }
+
+
     }
 }
