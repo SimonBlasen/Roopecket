f1fc914f15662a9d3b8e338a68eac3dd2bfbe1d4
ZiosXX <marc-bendtx@gmx.de>
2019-06-22 22:31:00 +0200

    added result screen

---
+++ Assets/Scripts/resultScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class resultScreen : MonoBehaviour
+{
+
+    public TextMeshProUGUI moneyText, totalDamageText, DamageText, totalTimeText, timeText;
+
+    public void showEndScreen()
+    {
+
+        moneyText.text = SavedGame.Money.ToString();
+        totalTimeText.text = SavedGame.CurrentLevelTime.ToString();
+
+    }
+
+}
