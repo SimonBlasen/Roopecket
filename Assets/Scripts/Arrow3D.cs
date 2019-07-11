4f6d952d6e51f537b9bb5a72b18683571e26136a
SimonBlasen <Weinfest123!>
2019-07-11 18:04:22 +0200

    Added Arrow3D, update to Unity 2019.1.9f1

---
+++ Assets/Scripts/Arrow3D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Arrow3D : MonoBehaviour
+{
+    public UnityEvent myUnityEvent;
+
+    Vector3 spawn;
+    Vector3 selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn = transform.position;
+        selected = spawn + new Vector3(0f, 0f, -0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
