5ef6be28868d149d1795bce25e39898f8464be7e
SimonBlasen <Weinfest123!>
2019-08-10 16:20:33 +0200

    Music plays throughout the scenes

---
+++ Assets/Scripts/MusicContinuous.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicContinuous : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+
+        Debug.Log("Dont destroy music");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
