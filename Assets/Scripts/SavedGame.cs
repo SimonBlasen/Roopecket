895c3831e3e88e5b6cdd4c26c61d581e3761f0f6
SimonBlasen <Weinfest123!>
2019-09-12 10:39:37 +0200

    Added Rubelrakete to the game

--- Assets/Scripts/SavedGame.cs
+++ Assets/Scripts/SavedGame.cs
@@ -266,6 +266,7 @@ public class SavedGame
             4000,   // 7er Rakete
             5000,   // Rakete 04
             5000,   // Rakete dongel
+            700,   // Rubelrakete
            
            
           
@@ -284,6 +285,7 @@ public class SavedGame
            1.2f,  //    1f, // 7er Rakete
            1.4f,  //  1.9f,  // Rakete 04
            4.0f,  //  1.9f,  // Rakete dongel
+           0.9f,  //  1.9f,  // Rubelrakete
        
           
             
