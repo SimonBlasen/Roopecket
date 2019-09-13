c7f874d3bc4a364357cf09021ba60529b8af366a
SimonBlasen <Weinfest123!>
2019-09-13 09:06:50 +0200

    Started working on stickers

---
+++ Assets/Scripts/StickerPlacer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickerPlacer : MonoBehaviour
+{
+    public Transform[] stickerPosses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
