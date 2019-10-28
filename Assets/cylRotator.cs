f29351f60a9db7a5b6f033ad2a18913c59593afd
ZiosXX <marc-bendtx@gmx.de>
2019-10-28 23:07:27 +0100

    New Rocket New Stage

---
+++ Assets/cylRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cylRotator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform earthTrans;
+    [SerializeField]
+    private float earthRotationSpeed;
+
+    private void Update()
+    {
+        for (int i = 0; i < 360; i++)
+        {
+
+            //earthTrans.rotation.y += earthRotationSpeed * Time.deltaTime;
+            earthTrans.rotation = Quaternion.Euler(earthTrans.rotation.eulerAngles.x, earthTrans.rotation.eulerAngles.y + earthRotationSpeed * Time.deltaTime, earthTrans.rotation.eulerAngles.z);
+        }
+    }
+}
---
+++ Assets/rocketPush.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class rocketPush : MonoBehaviour
+{
+
+    [SerializeField]
+    private KeyCode Rocketpush;
+    [SerializeField]
+    private Rigidbody RB;
+    [SerializeField]
+    private float ForceMulti;
+    [SerializeField]
+    private float pushLimit;
+    [SerializeField]
+    private GameObject ImpulseParticles;
+
+    [SerializeField]
+    TextMeshPro Ecounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        pushLimit = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(Rocketpush))
+        {
+            if (pushLimit >= 1)
+            {
+            RB.AddForce(new Vector3(0, ForceMulti, 0), ForceMode.Impulse);
+            GameObject ParticlePosition = Instantiate(ImpulseParticles);
+            ParticlePosition.transform.position = transform.position;
+            
+            pushLimit -= 1f;
+                Debug.Log(pushLimit);
+            }
+
+            Ecounter.text = pushLimit.ToString();
+       
+        }
+
+        
+
+    }
+
+}
